89129997362744dcdf0d65bc350a96dc56a0a218
Caleb Cushing <xenoterracide@gmail.com>
2011-09-21 15:03:06 -0500

    initial implementation of Locale::US::States
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/MooseX/Types/Locale/US/States.pm
+++ lib/MooseX/Types/Locale/US/States.pm
@@ -1,10 +1,37 @@
 package MooseX::Types::Locale::US::States;
-use 5.006;
+use 5.008;
 use strict;
 use warnings;
-BEGIN {
-	# VERSION
-}
+
+# VERSION
+
+use MooseX::Types -declare => [ qw( USStateName USStateCode ) ];
+
+use Locale::US;
+
+my $u = Locale::US->new;
+
+enum USStateName,
+	[ $u->all_state_names ]
+	;
+
+enum USStateCode,
+	[ $u->all_state_codes ]
+	;
+
+coerce USStateName,
+	from USStateCode,
+	via {
+		return $u->{code2state}{$_};
+	}
+	;
+
+coerce USStateCode,
+	from USStateName,
+	via {
+		return $u->{state2code}{$_};
+	}
+	;
 
 1;
 
